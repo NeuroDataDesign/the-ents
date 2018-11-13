7484b3b9fb2d8a26bfe5d236a49672e11e568150
v715 <vgopala4@jhu.edu>
2018-11-13 09:46:53 -0500

    download scripts

---
+++ src/data/download_data.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# download_data.sh
+# Created by Vivek Gopalakrishnan on 2018-11-13.
+# Email: vgopala4@jhu.edu
+# Copyright (c) 2018. All rights reserved.
+
+aws s3 cp s3://neurodatadesign/hbn/derivatives/graphs/ ../../data/raw/hbn/ --no-sign-request --recursive
+aws s3 cp s3://mrneurodata/data/HNU1/ndmg_0-0-48/graphs/ ../../data/raw/hnu1/ --no-sign-request --recursive
