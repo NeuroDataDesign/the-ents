5e07127c98b4d02174bb40068522747056a19536
v715 <vgopala4@jhu.edu>
2018-11-13 14:15:33 -0500

    final

--- notebooks/1.0-vg-runtime-simulation/phat.py
+++ notebooks/1.0-vg-runtime-simulation/phat.py
@@ -14,37 +14,45 @@ from src.data.edgelist_io import read_edgelist
 
 def estimate_phat(G):
 
+    # Get matrix
     A = nx.to_numpy_matrix(G)
     n = A.shape[0]
 
+    # Count upper triangular elements
     edges = np.count_nonzero(np.triu(A))
     num_upper = n * (n + 1) / 2
 
-    return edges / num_upper
+    # Estimate phat
+    return n, edges / num_upper
 
 
 def main(filepath='../../data/raw/hbn/'):
 
+    # Results dictionary
     results = {}
 
+    # List the altlases
     atlases = os.listdir(filepath)
 
+    # Iterate through atlases
     for atlas in atlases:
 
-        filelist = os.listdir(filepath + atlases)
-
         p_i = []
+        filelist = os.listdir(filepath + atlas)
+        print(atlas)
 
+        # Iterate through all files in a given atlas
         for file in filelist:
-            path = filepath + atlas + file
+            path = filepath + atlas + '/' + file
             G = read_edgelist(path)
             try:
-                p_i += [estimate_phat(G)]
+                n, phat_i = estimate_phat(G)
+                p_i += [phat_i]
             except:
                 pass
 
+        # Attach to dictionary
         p_hat = np.mean(p_i)
-
-        results[atlas] = p_hat
+        results[atlas] = [n, p_hat]
 
     return results
