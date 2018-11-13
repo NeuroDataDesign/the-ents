37092b2cf52c99378ab0443c562838232a789f30
v715 <vgopala4@jhu.edu>
2018-11-13 09:50:03 -0500

    blocked hnu download

--- src/data/download_data.sh
+++ src/data/download_data.sh
@@ -5,5 +5,8 @@
 # Email: vgopala4@jhu.edu
 # Copyright (c) 2018. All rights reserved.
 
+# Size: ~5 GB
 aws s3 cp s3://neurodatadesign/hbn/derivatives/graphs/ ../../data/raw/hbn/ --no-sign-request --recursive
-aws s3 cp s3://mrneurodata/data/HNU1/ndmg_0-0-48/graphs/ ../../data/raw/hnu1/ --no-sign-request --recursive
+
+# Size: ~10 GB
+# aws s3 cp s3://mrneurodata/data/HNU1/ndmg_0-0-48/graphs/ ../../data/raw/hnu1/ --no-sign-request --recursive
