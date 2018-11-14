b43c40f8c3c66039cc69350d9126455f5281d85e
ganesh.arvapalli@gmail.com <ganesh.arvapalli@gmail.com>
2018-11-13 23:52:55 -0500

    Init commit

---
+++ random_graphs/graph.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+# graph.py
+# Created by Vivek Gopalakrishnan on 2018-10-27.
+# Email:
+# Copyright (c) 2018. All rights reserved.
+
+"""
+Create random graphs for testing graph statistics on.
+"""
+
+import numpy as np
+import networkx as nx
+
+
+def random_graph(n, p, binary=True):
+    """
+    A function for simulating a random graph.
+    Paramaters
+    ----------
+        n: int
+            the number of vertices in the graph.
+        P: float, bounded between [0,1]
+            the probability of connection between any two nodes.
+        binary: boolean
+            whether to make a binary graph.
+    """
+
+    # Generate random adjacency matrix
+    s = (n, n)
+    A = np.zeros(s)
+
+    for i, j in zip(np.triu_indices(n)[0], np.triu_indices(n)[1]):
+        A[i, j] = np.random.binomial(1, p)
+
+    A += A.T
+    A[np.diag_indices_from(A)] /= 2
+    # print(A)
+
+    # Convert adjacency to nx object
+    G = nx.from_numpy_array(A)
+
+    return G
